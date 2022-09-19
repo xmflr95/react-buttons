8bb723580bf7cd0a24b4139964508481fb529191
ChangHui Han <xmflr95@naver.com>
2022-09-19 15:21:31 +0900

    react buttons example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import Button from './Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='buttons'>
+        <Button size="large" onClick={() => console.log("클릭됨!")}>BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button size="large" color="gray">BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button size="large" color="pink">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button size="large" color="blue" outline>BUTTON</Button>
+        <Button color="gray" outline>BUTTON</Button>
+        <Button size="small" color="pink" outline>BUTTON</Button>
+      </div>
+      <div className='buttons'>
+        <Button size="large" fullWidth>BUTTON</Button>
+        <Button size="large" color="gray" fullWidth>BUTTON</Button>
+        <Button size="large" color="pink" fullWidth>BUTTON</Button>
+      </div>
     </div>
   );
 }
